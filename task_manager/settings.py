c9c3fd56745e14691b20ea4a24396e98b7dda2b1
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-02-22 15:23:43 +0300

    6.5 completed

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -134,4 +134,5 @@ AUTH_USER_MODEL = "main.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
